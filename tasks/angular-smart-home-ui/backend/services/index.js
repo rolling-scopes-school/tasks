fedd661a1d4aa755e486a483685ad582080ad350
Pavel Razuvalau <pavel@razuvalov.com>
2025-07-24 09:02:15 +0200

    feat: remove be from tasks

--- tasks/angular-smart-home-ui/backend/services/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (files) => {
-  const db = require("./db")(files);
-  const routes = require("./routes")(files);
-  const middleware = require("./middleware")(files, db);
-  return { middleware, db, routes };
-};
